c06d3218ac403bb8da36543855243d6a031e394c
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-02-12 22:51:26 -0500

    Main

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.push File.expand_path('../lib', __FILE__)
 require 'identity-billing-reporter'
 
-puts "Hello"
+BillingReporter::GenerateReports.new.call(
+  dest_dir: ARGV[1],
+  year: ARGV[2].to_i,
+  month: ARGV[3].to_i,
+  auths_json: ARGV[4],
+  sp_yml: ARGV[5]
+)
