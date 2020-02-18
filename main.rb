b9d948774e342d264974a5bd6546ea4e80308f21
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-02-17 22:23:21 -0500

    Usage

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 $LOAD_PATH.push File.expand_path('../lib', __FILE__)
 require 'billing_reporter'
 
+if ARGV.length < 5
+  puts "Usage: ruby main.rb destination_directory year month total_monthly_auths.json sp.yml"
+  exit
+end
+
 BillingReporter::GenerateReports.new.call(
   dest_dir: ARGV[0],
   year: ARGV[1].to_i,
